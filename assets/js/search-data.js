eb1fcd87113e3e839fd7287e0aab35575af52514
priyankarbhattacharjee3 <priyankar.bhattacharjee3@gmail.com>
2026-01-31 05:08:16 +0000

    Deploying to gh-pages from @ priyankarbhattacharjee3/priyankarbhattacharjee3.github.io@ea73001b92e53e621688fa90de73ba691b493281 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,11 +445,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-attended-empower-2025-at-iit-delhi-sparkles-smile",
-          title: 'Attended Empower 2025 at IIT Delhi ! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-joined-aid-at-iiit-d-as-a-phd-student",
+            },},{id: "news-joined-aid-at-iiit-d-as-a-phd-student",
           title: 'Joined AID at IIIT-D as a PhD student',
+          description: "",
+          section: "News",},{id: "news-attended-empower-2025-at-iit-delhi-sparkles-smile",
+          title: 'Attended Empower 2025 at IIT Delhi ! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
